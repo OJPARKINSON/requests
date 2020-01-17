d86c1a6b3a3cb942b19e1ee90e6171cadbf4c12a
Oliver <olliepar01@gmail.com>
2020-01-17 14:37:34 +0000

    Getting data working

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ const app = express()
 
 const JSONcall = async () => {
     const response = await axios.get('http://ergast.com/api/f1/current/driverStandings.json');
-    console.log(response.data.MRData);
+    return response.data;
 }
 
 const XMLcall = async () => {
     const response = await axios.get('http://ergast.com/api/f1/current/driverStandings.xml');
-    console.log(response.data);
+    return response.data;
 }
 
-app.get('/JSONResponse', (res, req) => {
-    res.send()
-})
 
+app.route("/JSON")
+    .get(async (req, res) => res.send(await JSONcall()));
 
-JSONcall();
-XMLcall();
+app.route("/XML")
+    .get(async (req, res) => res.send(await XMLcall()));
 
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+app.listen(5000, () => console.log(`LISTENING ON PORT ${5000}`));
